20927447fcfc9d9e62eca30ae5a1c0c06d26d30f
Muhammet Özcan <mhmtozcn3737@gmail.com>
2021-12-23 17:52:52 +0300

    HTMLoluşturuldu

---
+++ app.js
@@ -0,0 +1,9 @@
+// To do eleman ekleme.
+// To do eleman çıkarma.
+//To do elemanlarını diziden sayfaya aktarma.
+// Local Storage ile çalışma.
+
+// ****************************************** //
+
+// To do eleman ekleme
+
