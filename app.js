4a0835e17ccc816f1002bac197106287bfcccf3a
Muhammet Özcan <mhmtozcn3737@gmail.com>
2021-12-23 23:20:10 +0300

    To do ekleme ve silme tamamlandı

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // To do eleman ekleme.
 // To do eleman çıkarma.
-//To do elemanlarını diziden sayfaya aktarma.
+// To do elemanlarını diziden sayfaya aktarma.
 // Local Storage ile çalışma.
 
 // ****************************************** //
@@ -15,27 +15,47 @@ const btnDeleteAll = document.querySelector('#btnDeleteAll');
 form.addEventListener('submit', addNewItem);
 
 function addNewItem(e) {
-    if(input.value === '') {
+    if (input.value.trim() === '') {
         alert('Lütfen bir değer giriniz')
     }
-    
-    const li = document.createElement('li');
-    li.classList = "list-group-item list-group-item-secondary";
-    li.appendChild(document.createTextNode(input.value));
+    else {
+        const li = document.createElement('li');
+        li.classList = "list-group-item list-group-item-secondary";
+        li.appendChild(document.createTextNode(input.value));
 
-    const a = document.createElement('a');
-    a.classList = "delete-item float-right";
-    a.setAttribute('href','#');
-    
-    const i = document.createElement('i');
-    i.classList = "fas fa-times";
-    
-    a.appendChild(i);
-    li.appendChild(a);
-    taskList.appendChild(li);
-    
+        const a = document.createElement('a');
+        a.classList = "delete-item float-right";
+        a.setAttribute('href', '#');
+
+        const i = document.createElement('i');
+        i.classList = "fas fa-times";
+
+        a.appendChild(i);
+        li.appendChild(a);
+        taskList.appendChild(li);
+    }
+
+
     input.value = '';
     e.preventDefault();
 }
 
-// To do tek bir elemanı ikona tıklayarak silme işlemi
+// To do silme işlemi
+
+taskList.addEventListener('click', deleteItem);
+
+function deleteItem(e) {
+    if (e.target.className === 'fas fa-times') {
+        e.target.parentElement.parentElement.remove();
+    }
+    e.preventDefault();
+}
+
+btnDeleteAll.addEventListener('click', deleteItemAll);
+
+function deleteItemAll(e) {
+    confirm('Hepsini silmek istediğinizden emin misiniz?')
+    taskList.innerHTML = '';
+    e.preventDefault();
+}
+
